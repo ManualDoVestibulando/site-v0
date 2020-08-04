4a62b36e7a2d2d4859375c4bd09c56efb91404a3
Guilherme Salustiano <jojo.guiss@hotmail.com>
2020-08-04 03:39:58 -0300

    Removendo campos computados

--- server/api/instituto/controllers/instituto.js
+++ server/api/instituto/controllers/instituto.js
@@ -1,28 +1,4 @@
 'use strict';
 const { sanitizeEntity } = require('strapi-utils');
 
-module.exports = {
-  /**
-   * Retrieve records.
-   *
-   * @return {Array}
-   */
-
-  async find(ctx) {
-    let entities;
-    
-    if (ctx.query.slug_) ctx.query.nome = decodeURI(ctx.query.slug_);
-    delete ctx.query.slug_;
-
-    if (ctx.query._q) {
-      entities = await strapi.services.instituto.search(ctx.query);
-    } else {
-      entities = await strapi.services.instituto.find(ctx.query);
-    }
-
-    return entities.map((entity) => {
-      entity.slug_ = encodeURI(entity.nome)
-      return sanitizeEntity(entity, { model: strapi.models.instituto });
-    });
-  },
-};
+module.exports = {};
