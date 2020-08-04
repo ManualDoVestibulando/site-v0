78d67876f56ae373f6d967190401623b7e09c8ed
Guilherme Salustiano <jojo.guiss@hotmail.com>
2020-08-04 00:59:34 -0300

    Add slug in curso backend

--- server/api/instituto/controllers/instituto.js
+++ server/api/instituto/controllers/instituto.js
@@ -1,8 +1,25 @@
 'use strict';
+const { sanitizeEntity } = require('strapi-utils');
 
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
+  /**
+   * Retrieve records.
+   *
+   * @return {Array}
+   */
 
-module.exports = {};
+  async find(ctx) {
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services.instituto.search(ctx.query);
+    } else {
+      entities = await strapi.services.instituto.find(ctx.query);
+    }
+    return entities.map((entity) => {
+      if (!entity.slug_) {
+        entity.slug_ = entity.sigla
+      }
+      return sanitizeEntity(entity, { model: strapi.models.instituto });
+    });
+  },
+};
