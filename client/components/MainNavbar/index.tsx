8827884320b173dede67a7b10834cd2e47c0cdd7
Eric Oliveira Gomes <ericog123456@gmail.com>
2020-08-05 17:57:54 -0300

    :sparkles: Add notas to navbar

--- client/components/MainNavbar/index.tsx
+++ client/components/MainNavbar/index.tsx
@@ -4,29 +4,34 @@ import Link from 'next/link';
 
 const MainNavbar = () => {
   return (
-      <Navbar bg="dark" variant="dark" expand="lg">
-        <Navbar.Brand href="#home">Manual do Vestibulando</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Link href="/">
-              <Nav.Link href="#home" className="ml-lg-5">
-                Início
-              </Nav.Link>
-            </Link>
-            <Link href="/redacoes">
-              <Nav.Link href="#redacoes" className="ml-lg-5">
-                Redações
-              </Nav.Link>
-            </Link>
-            <Link href="/sobre">
-              <Nav.Link href="#sobre" className="ml-lg-5">
-                Sobre
-              </Nav.Link>
-            </Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Navbar.Brand href="#home">Manual do Vestibulando</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Link href="/">
+            <Nav.Link href="#home" className="ml-lg-5">
+              Início
+            </Nav.Link>
+          </Link>
+          <Link href="/notas">
+            <Nav.Link href="#notas" className="ml-lg-5">
+              Pesquisar Notas
+            </Nav.Link>
+          </Link>
+          <Link href="/redacoes">
+            <Nav.Link href="#redacoes" className="ml-lg-5">
+              Redações
+            </Nav.Link>
+          </Link>
+          <Link href="/sobre">
+            <Nav.Link href="#sobre" className="ml-lg-5">
+              Sobre
+            </Nav.Link>
+          </Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
 
