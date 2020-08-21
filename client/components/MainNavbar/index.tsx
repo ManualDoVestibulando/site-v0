b4a7971737f653dc17de8d214b54342ace0f44b9
Eric Oliveira Gomes <ericog123456@gmail.com>
2020-08-21 16:09:45 -0300

    :lipstick: Update navbar style

--- client/components/MainNavbar/index.tsx
+++ client/components/MainNavbar/index.tsx
@@ -4,48 +4,52 @@ import Link from 'next/link';
 
 const MainNavbar = () => {
   return (
-    <Navbar expand="lg">
-      <Navbar.Brand href="#home">
-        <img
-          src="logo_laranja_manual_semfundo.svg"
-          width="80"
-          height="80"
-          className="d-inline-block align-top"
-          alt="Manual do Vestibulando"
-        />
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Link href="/">
-            <Nav.Link href="#home" className="ml-lg-5">
-              Início
-            </Nav.Link>
-          </Link>
-          <Link href="/notas">
-            <Nav.Link href="#notas" className="ml-lg-5">
-              Pesquisar Notas
-            </Nav.Link>
-          </Link>
-          <Link href="/redacoes">
-            <Nav.Link href="#redacoes" className="ml-lg-5">
-              Redações
-            </Nav.Link>
-          </Link>
-          <Link href="/sobre">
-            <Nav.Link href="#sobre" className="ml-lg-5">
-              Sobre
-            </Nav.Link>
-          </Link>
-        </Nav>
-      </Navbar.Collapse>
-      <style jsx>{`
-        .home {
-          color: black;
-          background-color: coral;
+    <React.Fragment>
+      <Navbar expand="lg" className="barra-superior">
+        <Link href="/">
+          <Navbar.Brand href="#home" className="m-0 p-0">
+            <img
+              src="logo_laranja_manual_fundobranco.svg"
+              width="80"
+              height="80"
+              className="d-inline-block align-top"
+              alt="Manual do Vestibulando"
+            />
+          </Navbar.Brand>
+        </Link>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Link href="/">
+              <Nav.Link href="#home" className="ml-lg-5 my-2">
+                INÍCIO
+              </Nav.Link>
+            </Link>
+            <Link href="/notas">
+              <Nav.Link href="#notas" className="ml-lg-5 my-2">
+                PESQUISAR NOTAS
+              </Nav.Link>
+            </Link>
+            <Link href="/redacoes">
+              <Nav.Link href="#redacoes" className="ml-lg-5 my-2">
+                REDAÇÕES
+              </Nav.Link>
+            </Link>
+            <Link href="/sobre">
+              <Nav.Link href="#sobre" className="ml-lg-5 my-2">
+                SOBRE
+              </Nav.Link>
+            </Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <style jsx global>{`
+        .barra-superior {
+          background-color: #ff8a00;
+          font-weight: bold;
         }
       `}</style>
-    </Navbar>
+    </React.Fragment>
   );
 };
 
